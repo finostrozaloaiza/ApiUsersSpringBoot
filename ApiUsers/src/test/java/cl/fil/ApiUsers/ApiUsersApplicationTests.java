ae6d7312a6943e38dd1c0b9652081d0385843cfd
Felipe Inostroza <finostroza@protonmail.com>
2022-10-24 20:54:08 -0300

    First commit
    Initial commit, create packages

---
+++ ApiUsers/src/test/java/cl/fil/ApiUsers/ApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.fil.ApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
